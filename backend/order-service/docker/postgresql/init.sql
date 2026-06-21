ffba9aa711262548883ada122f32605ddb7398d9
Mousum Xxx <62622428+Mousum2002@users.noreply.github.com>
2026-06-21 16:01:00 +0200

    PosgreserverServer setup done

---
+++ backend/order-service/docker/postgresql/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS orders (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  customer_id UUID NOT NULL,
+  products JSONB NOT NULL,
+  total_price NUMERIC(10,2) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
