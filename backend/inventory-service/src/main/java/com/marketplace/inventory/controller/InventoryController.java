80dcd9075cf7b4aec1b75715c452944b5550e4a4
Mousum Xxx <62622428+Mousum2002@users.noreply.github.com>
2026-06-30 00:09:10 +0200

    modified inventory service to accept multiple productsID to cheack

--- backend/inventory-service/src/main/java/com/marketplace/inventory/controller/InventoryController.java
+++ backend/inventory-service/src/main/java/com/marketplace/inventory/controller/InventoryController.java
@@ -1,21 +1,25 @@
 package com.marketplace.inventory.controller;
 
-import com.marketplace.inventory.service.Service;
-import lombok.RequiredArgsConstructor;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
+import com.marketplace.inventory.service.Service;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController("/api/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
-    private final Service service;
-    @GetMapping
-    public ResponseEntity<Boolean> isInInventory(@RequestParam UUID productId, @RequestParam Integer quantity){
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.isInInventory(productId,quantity));
-    }
+  private final Service service;
+
+  @GetMapping
+  public ResponseEntity<Map<UUID, Boolean>> isInInventory(@RequestBody Map<UUID, Integer> products) {
+    return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.isInInventory(products));
+  }
 }
--- backend/inventory-service/src/main/java/com/marketplace/inventory/service/Service.java
+++ backend/inventory-service/src/main/java/com/marketplace/inventory/service/Service.java
@@ -1,18 +1,23 @@
 package com.marketplace.inventory.service;
 
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import com.marketplace.inventory.repo.Repository;
-import lombok.RequiredArgsConstructor;
 
-import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 
 @org.springframework.stereotype.Service
 @RequiredArgsConstructor
 public class Service {
 
-    private final Repository repo;
-    public boolean isInInventory(UUID productId, Integer quantity){
-        return repo.existsByProductIdAndQuantityIsGreaterThanEqual(productId,quantity);
-    }
+  private final Repository repo;
+
+  public Map<UUID, Boolean> isInInventory(Map<UUID, Integer> products) {
+    return products.entrySet().stream()
+        .collect(Collectors.toMap(Map.Entry::getKey,
+            entry -> repo.existsByProductIdAndQuantityIsGreaterThanEqual(entry.getKey(), entry.getValue())));
+  }
 
 }
