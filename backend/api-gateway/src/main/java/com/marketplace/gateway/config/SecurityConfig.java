2e4f602ac896f42958f331a221aaa6151d55266d
Mousum Xxx <62622428+Mousum2002@users.noreply.github.com>
2026-07-09 13:39:55 +0200

    api gateway partially done

---
+++ backend/api-gateway/src/main/java/com/marketplace/gateway/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.marketplace.gateway.config;
+
+import java.util.Base64;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  @Value("${jwt.secret}")
+  private String jwtSecret;
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(request -> request
+            .requestMatchers("/api/auth/login", "/api/auth/resgister", "/api/product/all").permitAll()
+            .anyRequest().authenticated())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())))
+        .build();
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    byte[] decodedKey = Base64.getDecoder().decode(jwtSecret);
+    SecretKey secretKey = new SecretKeySpec(decodedKey, "HmacSHA256");
+
+    return NimbusJwtDecoder.withSecretKey(secretKey)
+        .macAlgorithm(MacAlgorithm.HS256).build();
+  }
+}
